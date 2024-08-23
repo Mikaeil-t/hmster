f1e1a8f89f60253fcd6200f3bdbaf198ee1d277f
Mikaeil-t <126967014+Mikaeil-t@users.noreply.github.com>
2024-08-23 10:18:48 +0330

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
             appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
             promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
             timing: 12000, // 21 seconds
-            attempts: 20,
+            attempts: 22,
         },
         2: {
             name: 'Chain Cube 2048',
@@ -18,36 +18,36 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'My Clone Army',
             appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
             promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
-            timing: 99000, // 120 seconds
-            attempts: 10,
+            timing: 12000, // 120 seconds
+            attempts: 12,
         },
         4: {
             name: 'Train Miner',
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
             timing: 120000, // 20 seconds
-            attempts: 9,
+            attempts: 10,
         },
         5: {
             name: 'Merge Away',
             appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
             promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
             timing: 12000, // 21 seconds
-            attempts: 11,
+            attempts: 15,
         },
         6: {
             name: 'Twerk Race 3D',
             appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
             promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
             timing: 12000, // 23 seconds
-            attempts: 12,
+            attempts: 15,
         },
         7: {
             name: 'Polysphere',
             appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
             promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
             timing: 12000, // 17 seconds
-            attempts: 15,
+            attempts: 19,
         }
     };
 
