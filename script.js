c80b21ec30d4723f2d833f6dea5498824a4598ae
Mikaeil-t <126967014+Mikaeil-t@users.noreply.github.com>
2024-08-23 09:10:39 +0330

    Update script.js

--- script.js
+++ script.js
@@ -4,50 +4,50 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Riding Extreme 3D',
             appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
             promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
-            timing: 21000, // 21 seconds
-            attempts: 22,
+            timing: 12000, // 21 seconds
+            attempts: 20,
         },
         2: {
             name: 'Chain Cube 2048',
             appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
             promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
-            timing: 20000, // 20 seconds
+            timing: 12000, // 20 seconds
             attempts: 10,
         },
         3: {
             name: 'My Clone Army',
             appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
             promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
-            timing: 120000, // 120 seconds
-            attempts: 12,
+            timing: 99000, // 120 seconds
+            attempts: 10,
         },
         4: {
             name: 'Train Miner',
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
-            timing: 20000, // 20 seconds
-            attempts: 10,
+            timing: 120000, // 20 seconds
+            attempts: 9,
         },
         5: {
             name: 'Merge Away',
             appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
             promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
-            timing: 21000, // 21 seconds
-            attempts: 15,
+            timing: 12000, // 21 seconds
+            attempts: 11,
         },
         6: {
             name: 'Twerk Race 3D',
             appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
             promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-            timing: 23000, // 23 seconds
-            attempts: 15,
+            timing: 12000, // 23 seconds
+            attempts: 12,
         },
         7: {
             name: 'Polysphere',
             appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
             promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
-            timing: 17000, // 17 seconds
-            attempts: 19,
+            timing: 12000, // 17 seconds
+            attempts: 15,
         }
     };
 
